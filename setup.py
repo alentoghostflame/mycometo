c03ca20719321c451f0a2e086966699b43938037
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2023-03-02 20:50:46 -0600

    Bumps up version
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="mycometo",
-    version="0.0.1",
+    version="0.0.2",
     description="A multi-layer IPC framework for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
